c562c695b42f44ed477bba52c0a0452183aca8e5
One Hundred Proof <one.hundred.proof@proton.me>
2022-10-31 08:02:36 +1000

    Remove defunct Infura API key and get user to provide

--- run-forge.sh
+++ run-forge.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -v
 
 if [ $# -ge 1 ]; then
    FLAGS=$@
@@ -7,6 +6,25 @@ else
    FLAGS="-vv"
 fi
 
+if [ "$MAINNET_FORK_URL" = "" -a "$INFURA_API_KEY" = "" -a "$ALCHEMY_API_KEY" = "" ]; then
+   echo "Please export MAINNET_FORK_URL, INFURA_API_KEY or ALCHEMY_API_KEY to continue"
+   echo "e.g."
+   echo "   export MAINNET_FORK_URL=https://mainnet.infura.io/v3/deadbeefdeadbeefdeadbeefdeadbeef"
+   echo "or the slightly shorter:"
+   echo "   export INFURA_API_KEY=deadbeefdeadbeefdeadbeefdeadbeef"
+   echo "or even:"
+   echo "   export ALCHEMY_API_KEY=deadbeefdeadbeefdeadbeefdeadbeef"
+   exit 1
+fi
+
+if [ "$INFURA_API_KEY" != "" ]; then
+   export MAINNET_FORK_URL="https://mainnet.infura.io/v3/$INFURA_API_KEY"
+fi
+
+if [ "$ALCHEMY_API_KEY" != "" ]; then
+   export MAINNET_FORK_URL="https://eth-mainnet.alchemyapi.io/v2/$ALCHEMY_API_KEY"
+fi
+
 ## Sep-02-2022 01:51:09 AM +UTC
-forge test --fork-url https://mainnet.infura.io/v3/88c04cb1d87c4a1d863bc878516e6e34 --fork-block-number 15456374 --revert-strings debug $FLAGS
+forge test --fork-url "$MAINNET_FORK_URL" --fork-block-number 15456374 --revert-strings debug $FLAGS
 
